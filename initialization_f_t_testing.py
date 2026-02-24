9a078d13b1b9a68aa288180e3e93dd70559a715b
calebn2813-source <cnomi@calpoly.edu>
2026-02-24 09:37:20 -0800

    reeeeee

--- initialization_f_t_testing.py
+++ initialization_f_t_testing.py
@@ -1,6 +1,7 @@
 from A_initialization_alpha import initialization
 import csv
 import numpy as np
+import time
 
 # Define the filename
 filename = "tfr_data_log.csv"
@@ -10,18 +11,20 @@ with open(filename, mode='w', newline='') as csvfile:
     # Define the headers
     fieldnames = ['f_current', 'time_delay', 'i', 'R_th', 'T_0_1']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
     # Write the header row
     writer.writeheader()
 
-    for i in range(8):
-        f_current = 1.01 + (i * 0.005) # f_current ranges from 1.01 to 1.05
-        for j in range(11):
-            time_delay = 0.25 + (j * 0.25) # time_delay ranges from 0.25s to 3s
+for i in range(6):
+    f_current = 1.01 + (i * 0.005) # f_current ranges from 1.01 to 1.05
+    for j in range(10):
+        time_delay = 0.05 + (j * 0.2) # time_delay ranges from 0.25s to 3s
 
-            i, R_th, T_0_1 = initialization(f_current, time_delay) # Run full initialization script
+        i, R_th, T_0_1 = initialization(f_current, time_delay) # Run full initialization script
 
-            # Log the data for the current iteration
+        # Log the data for the current iteration
+
+        with open(filename, mode='a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow({
                 'f_current': round(f_current, 4),
                 'time_delay': round(time_delay, 2),
@@ -29,5 +32,8 @@ with open(filename, mode='w', newline='') as csvfile:
                 'R_th': round(R_th, 2),
                 'T_0_1': round(T_0_1, 4),
             })
+
+        
+        time.sleep(15)
 
         print(f"Data successfully logged to {filename} for f_current = {f_current} and time_delay = {time_delay}\n")
